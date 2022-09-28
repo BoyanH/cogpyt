2fe939a984fd667f38b69676136cd3cea05829a3
Boyan Hristov <hristov@excentos.com>
2022-09-29 00:16:56 +0200

    bump version to 0.0.2 for the next development iteration

--- cogpyt/__about__.py
+++ cogpyt/__about__.py
@@ -6,6 +6,6 @@ __title__ = 'cogpyt'
 __author__ = 'Boyan Hristov'
 __description__ = "Code Generation in (pure) Python"
 __url__ = 'https://github.com/BoyanH/cogpyt'
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __copyright__ = 'Copyright 2022 Boyan Hristov'
 __licence__ = 'MIT'
